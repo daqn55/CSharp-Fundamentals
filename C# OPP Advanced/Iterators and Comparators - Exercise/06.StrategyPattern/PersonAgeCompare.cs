101770ea47747ad10a5d8ca5b37d1b652ed96d0b
daqn <daqn_5@mail.bg>
2018-04-01 19:38:55 +0300

    homework

---
+++ C# OPP Advanced/Iterators and Comparators - Exercise/06.StrategyPattern/PersonAgeCompare.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+public class PersonAgeCompare : IComparer<Person>
+{
+    public int Compare(Person x, Person y)
+    {
+        int result = x.Age.CompareTo(y.Age);
+
+        return result;
+    }
+}
+
--- C# OPP Advanced/Iterators and Comparators - Exercise/06.StrategyPattern/Program.cs
+++ C# OPP Advanced/Iterators and Comparators - Exercise/06.StrategyPattern/Program.cs
@@ -1,10 +1,35 @@
 ﻿using System;
-
+using System.Linq;
+using System.Collections.Generic;
 
 public class Program
 {
     static void Main(string[] args)
     {
+        int n = int.Parse(Console.ReadLine());
+
+        SortedSet<Person> peopleNameSorted = new SortedSet<Person>(new PersonNameCompare());
+        SortedSet<Person> peopleAgeSorted = new SortedSet<Person>(new PersonAgeCompare());
+
+        for (int i = 0; i < n; i++)
+        {
+            var input = Console.ReadLine().Split();
+
+            Person person = new Person(input[0], int.Parse(input[1]));
+
+            peopleNameSorted.Add(person);
+            peopleAgeSorted.Add(person);
+        }
+
+        foreach (var p in peopleNameSorted)
+        {
+            Console.WriteLine(p.Name + " " + p.Age);
+        }
+
+        foreach (var p in peopleAgeSorted)
+        {
+            Console.WriteLine(p.Name + " " + p.Age);
+        }
     }
 }
 
