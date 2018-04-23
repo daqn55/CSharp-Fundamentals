6b8b2c278c6b76a049c8bf714ee9f5ac231daa26
daqn <daqn_5@mail.bg>
2018-04-23 19:27:43 +0300

    ExamPreparation

---
+++ C# Advanced/ExamPreparation1/03.CryptoBlockchain/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace _03.CryptoBlockchain
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            var sb = new StringBuilder();
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+                sb.Append(input);
+            }
+
+            var fullBlokchain = sb.ToString();
+
+            string pattern = @"(?:({[^\[\]{}]+})|(\[[^{}\[\]]+\]))";
+            Regex regex = new Regex(pattern);
+            MatchCollection matches = regex.Matches(fullBlokchain);
+
+            //List<int> lenghtOfBlock = new List<int>();
+            string letterToPrint = string.Empty;
+            foreach (Match m in matches)
+            {
+                var block = m.ToString();
+
+                //lenghtOfBlock.Add(block.Length);
+
+                var count = 0;
+                var numbersInStr = string.Empty;
+                foreach (var b in block)
+                {
+                    var isNum = char.IsDigit(b);
+                    if (isNum)
+                    {
+                        count++;
+                        numbersInStr += b;
+                    }
+                }
+
+                if (count % 3 == 0)
+                {
+                    for (int i = 0; i < numbersInStr.Length; i+=3)
+                    {
+                        string numToParse = numbersInStr[i].ToString() + numbersInStr[i + 1] + numbersInStr[i + 2];
+                        int num = int.Parse(numToParse);
+
+                        letterToPrint += Convert.ToChar(num - block.Length);
+                    }
+                }
+            }
+
+            Console.WriteLine(letterToPrint);
+        }
+    }
+}
---
+++ C# Advanced/ExamPreparation1/04.HitList/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04.HitList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int targetIndex = int.Parse(Console.ReadLine());
+
+            Dictionary<string, Dictionary<string, string>> allInfo = new Dictionary<string, Dictionary<string, string>>();
+            string input = string.Empty;
+            while ((input = Console.ReadLine()) != "end transmissions")
+            {
+                string[] infoName = input.Split(new string[] { "=", ";" }, StringSplitOptions.RemoveEmptyEntries);
+                string name = infoName[0];
+                Dictionary<string, string> personData = new Dictionary<string, string>();
+
+                if (!allInfo.ContainsKey(name))
+                {
+                    allInfo.Add(name, personData);
+                }
+
+                foreach (var i in infoName.Skip(1))
+                {
+                    var info = i.Split(':');
+                    if (allInfo[name].ContainsKey(info[0]))
+                    {
+                        allInfo[name][info[0]] = info[1];
+                    }
+                    else
+                    {
+                        allInfo[name].Add(info[0], info[1]);
+                    }
+                }
+            }
+
+            string nameToKill = Console.ReadLine().Split()[1];
+
+            int infoIndex = 0;
+            Console.WriteLine($"Info on {nameToKill}:");
+            foreach (var p in allInfo.Where(z => z.Key == nameToKill))
+            {
+                foreach (var v in p.Value.OrderBy(x => x.Key))
+                {
+                    infoIndex += v.Key.Length + v.Value.Length;
+                    Console.WriteLine($"---{v.Key}: {v.Value}");
+                }
+            }
+
+            Console.WriteLine($"Info index: {infoIndex}");
+            if (infoIndex >= targetIndex)
+            {
+                Console.WriteLine("Proceed");
+            }
+            else
+            {
+                Console.WriteLine($"Need {targetIndex - infoIndex} more info.");
+            }
+        }
+    }
+}
